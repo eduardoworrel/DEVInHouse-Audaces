c91bf99d050349df9f2843549441b6e62cb9efca
eduardoworrel <=>
2022-03-15 21:56:03 -0300

    aula 15/03

---
+++ NAAULA/JAVASCRIPT/eventos.js
@@ -0,0 +1,10 @@
+const div = document.createEelement("div");
+
+const div2 = document.querySelector("#caixa")
+
+div.innerText = "texto interno";
+
+div.style.backgroundColor = "blue";
+
+div2.append(div)
+
