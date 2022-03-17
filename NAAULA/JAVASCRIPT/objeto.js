560d957f94744b74d57397c3a095aaf06190b96b
eduardoworrel <=>
2022-03-16 22:20:42 -0300

    aula 16/03

---
+++ NAAULA/JAVASCRIPT/objeto.js
@@ -0,0 +1,13 @@
+let obj = {
+    nome: "eduardo",
+    menu: document.querySelector(".menu"),
+    fazAlgo: function () {
+        alert("fiz")
+    }
+
+};
+
+obj.idade = 25;
+
+
+obj.menu.style.backgroundColor = "black";
